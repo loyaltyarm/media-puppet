6d6d5b8e71466d8e8eee83bbcdfcd98b37d99439
Cobbler <loyaltyarm@gmail.com>
2018-08-07 05:02:59 +0000

    fix setup of source, installation of plexmediaserver

--- modules/packages/manifests/init.pp
+++ modules/packages/manifests/init.pp
@@ -5,15 +5,21 @@ class packages() {
   apt::source { 'plexmediaserver':
     comment  => 'A repository for auto-updating Plex Media Server',
     location => 'https://downloads.plex.tv/repo/deb',
-    release  => 'stable',
-    repos    => 'public main',
+    release  => 'public',
+    repos    => 'main',
     key      => {
       'id'     => 'CD665CBA0E2F88B7373F7CB997203C7B3ADCA79D',
-      'server' => 'https://downloads.plex.tv/plex-keys/PlexSign.key',
+      'source' => 'https://downloads.plex.tv/plex-keys/PlexSign.key',
     },
     include  => {
       'deb' => true,
     },
+  }
+
+  package {
+    ['plexmediaserver']:
+    ensure   => installed,
+    provider => 'apt';
   }
 }
 
