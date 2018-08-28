04760358ce4eded2f6b232392bf5c3e6554478c2
Nick Cobb <loyaltyarm@gmail.com>
2018-08-28 07:01:35 +0000

    add notify_update to sources list config

--- modules/packages/manifests/init.pp
+++ modules/packages/manifests/init.pp
@@ -3,23 +3,23 @@ class packages() {
   include apt
 
   apt::source { 'plexmediaserver':
-    comment  => 'A repository for auto-updating Plex Media Server',
-    location => 'https://downloads.plex.tv/repo/deb',
-    release  => 'public',
-    repos    => 'main',
-    key      => {
-      'id'     => 'CD665CBA0E2F88B7373F7CB997203C7B3ADCA79D',
-      'source' => 'https://downloads.plex.tv/plex-keys/PlexSign.key',
+    comment       => 'A repository for auto-updating Plex Media Server',
+    location      => 'https://downloads.plex.tv/repo/deb',
+    release       => 'public',
+    repos         => 'main',
+    notify_update => 'true',
+    key           => {
+      'id'          => 'CD665CBA0E2F88B7373F7CB997203C7B3ADCA79D',
+      'source'      => 'https://downloads.plex.tv/plex-keys/PlexSign.key',
     },
-    include  => {
-      'deb' => true,
+    include       => {
+      'deb'         => true,
     },
-  } ->
+  }
   package {
     ['plexmediaserver']:
-    ensure   => installed,
-    provider => 'apt',
-    notify => Exec['apt_update'];
+    ensure        => installed,
+    provider      => 'apt';
   }
 }
 
